31449e4901b5f2a9cd5e62842877a2fe8fefabb5
juigawali <65781992+juigawali@users.noreply.github.com>
2021-01-13 19:46:50 +0530

    Create fday.c
    Programming in c language

---
+++ juigawali/fday.c
@@ -0,0 +1,14 @@
+ Date: 13 Jan,2020.
+
+#include <stdio.h>
+ 
+int main()
+{
+    int l,b;
+    float area;
+    printf("Enter the value of l and b:\n");
+    scanf("%d%d",&l,&b);
+    area=l*b;
+    printf("Area is:%f",area);
+    return 0;
+}
