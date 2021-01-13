74b091d502e097d735c5797e723148276c5d62b4
Durwa <65773424+Durwa@users.noreply.github.com>
2021-01-13 16:52:55 +0530

    Update sub.c

--- Durwapatil/sub.c
+++ Durwapatil/sub.c
@@ -1,1 +1,10 @@
-fdgfght
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int n1, n2, diff;
+  printf("Enter two numbers : \n");
+  scanf("%d %d", &n1, &n2);
+  diff = n1 - n2;
+  printf("%d - %d = %d", n1, n2, diff);
+  return 0;
+}
