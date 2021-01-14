7679f350a96f24a1feadddb9617a197f4caa36e6
juigawali <65781992+juigawali@users.noreply.github.com>
2021-01-14 23:21:36 +0530

    Create sday.c
    added new program

---
+++ juigawali/sday.c
@@ -0,0 +1,14 @@
+# Date: 14 jan,2020.
+
+#include<stdio.h>
+int main()
+{
+   int num;
+   printf("Enter a number");
+   scanf("%d",&num);
+   
+   printf("Now I am letting you on a secret...\n");
+   printf("You have just entered the number %d\n",num);
+   return 0;
+}   
+   
