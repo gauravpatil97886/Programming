92f440011a4f5a2274bb39305339166015bf32be
Durwa <65773424+Durwa@users.noreply.github.com>
2021-01-15 16:28:42 +0530

    Update sub.c

--- Durwapatil/sub.c
+++ Durwapatil/sub.c
@@ -1,10 +1,16 @@
+//Date : 13 Jan 2021
+
 #include<stdio.h>
 #include<stdlib.h>
-int main(){
+int main()
+{
   int n1, n2, diff;
+  
   printf("Enter two numbers : \n");
   scanf("%d %d", &n1, &n2);
+  
   diff = n1 - n2;
+  
   printf("%d - %d = %d", n1, n2, diff);
   return 0;
 }
