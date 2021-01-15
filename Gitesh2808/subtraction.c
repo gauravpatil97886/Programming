39fc956b381ac05f7298c8d654bfdd2a71d5d557
Gitesh2808 <65801503+Gitesh2808@users.noreply.github.com>
2021-01-15 19:41:52 +0530

    subtraction

--- Gitesh2808/subtraction.c
+++ Gitesh2808/subtraction.c
@@ -1,10 +1,16 @@
+//C Program to calculate subtraction of two numbers..
+
 #include<stdio.h>
 #include<stdlib.h>
-int main() {
+int main()
+{
   int a, b, sub;
+  
   printf("Enter two numbers :\n");
   scanf("%d %d", &a, &b);
+  
   sub = a - b;
   printf("Subtraction of two numbers is :", sub);
+  
   return 0;
 }
