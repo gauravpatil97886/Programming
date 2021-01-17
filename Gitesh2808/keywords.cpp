0d12a231d0f4e803eee3ff1094e606012991cb1f
Gitesh2808 <65801503+Gitesh2808@users.noreply.github.com>
2021-01-17 23:24:59 +0530

    keywords in C++

---
+++ Gitesh2808/keywords.cpp
@@ -0,0 +1,26 @@
+// Date : 17 Jan 2021
+
+
+//Keywords in C++ :
+
+//    A keyword is a reserved word. There are total 62 keywords in all in C++ Language. They are as follows :
+    
+//    The list of following 32 keywords in C++ language are also available in C language.
+    
+    auto          break          case          char          const          continue          default          do
+    double        else           enum          extern        float          for               goto             if
+    int           long           register      return        short          signed            sizeof           static
+    struct        switch         typedef       union         unsigned       void              volatile         while
+                  
+        
+        
+        
+//    The following list of 30 keywords in C++ language which are not available in C language.        
+                  
+    asm               dynamic_cast             namespace               reinterpret_cast           bool
+    explicit          new                      static_cast             false                      catch
+    operator          template                 friend                  private                    class
+    this              inline                   public                  throw                      const_cast
+    delete            mutable                  protected               true                       try
+    typeid            typename                 using                   virtual                    wchar_t
+    
