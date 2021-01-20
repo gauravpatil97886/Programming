0446b7bf4df8b6ebf03bed18fdd3db43f36b8353
Gitesh2808 <65801503+Gitesh2808@users.noreply.github.com>
2021-01-20 20:51:54 +0530

    Create circle-area.java

---
+++ Gitesh2808/Java/circle-area.java
@@ -0,0 +1,23 @@
+// Date : 20 Jan 2021
+
+// Java program to display the area of circle 
+
+            import java.io.*;
+            class circle
+            {
+              public static void main(String args[])
+              {
+                try 
+                {
+                  DataInputStream x=new DataInputStream(System.in);
+                  int r;
+                  float ac;
+                  System.out.print("\nEnter radius : \n");
+                  r = Integer.parseInt(x.readLine());
+                  ac = (float)3.14 * r * r;
+                  System.out.print("\nThe area of the circle is "+ ac);
+                }
+                catch(Exception e)
+                {}
+              }
+            }
