0928911d8c4d1f0c7ef7e975c0280494dd02989a
Ganesh-0087 <66048631+Ganesh-0087@users.noreply.github.com>
2021-01-20 23:26:27 +0530

    Armstrong number program

---
+++ Ganeshsonawane/c/Armstrong.c
@@ -0,0 +1,37 @@
+// Date: 20 Jan 2021
+
+// C Program to determine whether a number is Armstrong number or not :-
+
+        #include <stdio.h>
+        #include <stdlib.h>
+        
+        int main()
+        {
+          int no, no1, a, b, c, rev;
+          printf("Enter any three digit number : \n");
+          scanf("%d ", &no);
+          no1 = no;
+          a = no % 10;
+          no = no / 10;
+          b = no % 10;
+          no = no / 10;
+          c = no;
+          rev = (a * a * a) + (b * b * b) + (c * c * c);
+          if(rev == no1)
+          {
+            printf("The given number is armstrong number");
+          }
+          else
+          {
+            printf("The given number is not armstrong number");
+          }
+          return 0;
+        }
+
+/*
+Output :
+    Enter any three digit number : 
+    153
+    
+    The given number is armstrong number
+*/
