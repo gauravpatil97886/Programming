09d7acab75b2d569803782a6c87ad029ce66d205
Gitesh2808 <65801503+Gitesh2808@users.noreply.github.com>
2021-01-20 20:37:44 +0530

    swapping of two numbers in java

---
+++ Gitesh2808/Java/swapping.java
@@ -0,0 +1,18 @@
+// Date : 20 Jan 2021
+
+// Java program to display the swapping of two numbers 
+
+        class swap
+        {
+          public static void main(String args[])
+          {
+            int a, b, c;
+            a = 10;
+            b = 20;
+            c = a;
+            a = b;
+            b = c;
+            System.out.print(a);
+            System.out.print(b);
+          }
+        }
