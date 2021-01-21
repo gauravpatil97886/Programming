756e066891722c2e66d0234b153a2c58656dddfe
Priyankawakalkar <65772811+Priyankawakalkar@users.noreply.github.com>
2021-01-21 18:24:21 +0530

    Update program.java

--- Priyankawakalkar/program.java
+++ Priyankawakalkar/program.java
@@ -32,3 +32,35 @@ int a,b,c;
   System.out.print("sub is ",c);
 }
 }
+
+## swap using third variable swap.java
+class swap
+{
+public static void main(String args[])
+{
+int a,b,c;
+  a=10;
+  b=20;
+  c=a;
+  a=b;
+  b=c;
+  System.out.print(a);
+  System.out.print(b);
+}
+}
+
+## swap without using third variable
+class swap
+{
+  public static void main(String args[])
+  {
+    int a,b,c;
+    a=10;
+    b=20;
+    a=a+b;     //a=a*b;
+    b=a-b;     //b=a/b;
+    a=a-b;     //a=a/b;
+    System.out.print(a);
+    System.out.print(b);
+  }
+}
