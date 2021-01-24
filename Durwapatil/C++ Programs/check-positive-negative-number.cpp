9c93ab95d90137f6c6d56ff5460b98b1ca44b6e3
Durwa <65773424+Durwa@users.noreply.github.com>
2021-01-24 14:46:35 +0530

    check whether the number is positive or negative

---
+++ Durwapatil/C++ Programs/check-positive-negative-number.cpp
@@ -0,0 +1,45 @@
+// Date : 24J Jan 2021
+
+// CPP program to check whether the number is positive or negative
+
+
+
+#include <iostream>
+using namespace std;
+
+class ab
+{
+    int a;
+    public : int no();
+};
+int ab :: no()
+{
+    cout << "Enter any number : \n";
+    cin >> a;
+    if(a > 0)
+    {
+        cout << "Number is positive";
+    }
+    else if(a < 0)
+    {
+        cout << "Number is negative";
+    }
+    else
+    {
+        cout << "Number is zero";
+    }
+}
+int main()
+{
+    ab st;
+    st.no();
+}
+
+
+/*
+Output : 
+    Enter any number : 
+    5
+    
+    Number is positive
+*/
