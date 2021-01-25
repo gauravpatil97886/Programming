79c7d07619a74930badfbdb18bbb023641288cef
Gitesh2808 <65801503+Gitesh2808@users.noreply.github.com>
2021-01-25 21:32:47 +0530

    display the marksheet using array

---
+++ Gitesh2808/C/array-of-marksheet.c
@@ -0,0 +1,59 @@
+// Date : 25 Jan 2021 
+
+// C program to display the marksheet
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int rn[10], m1[10], m2[10], tot[10], n, i;
+    float per[10];
+    printf("Enter the array size : \n");
+    scanf("%d", &n);
+    for(i = 0; i < n; i++)
+    {
+        printf("Enter the roll no, m1, m2 : \n");
+        scanf("%d %d %d", &rn[i], &m1[i], &m2[i]);
+        tot[i] = m1[i] + m2[i];
+        per[i] = tot[i] / 2.0;
+    }
+    printf("\n\t**********MARKSHEET OF DIPLOMA FIRST YEAR**********\n");
+    printf("\n\nROLL NO\t\tMATHS\t\tENGLISH\t\tTOTAL\t\tPERCENTAGE\n\n\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%.2f\n\n\n", rn[i], m1[i], m2[i], tot[i], per[i]);
+    }
+    return 0;
+}
+
+
+/*
+Output : 
+    Enter the array size : 
+    2
+    
+    Enter the roll no, m1, m2 : 
+    1
+    85
+    94
+
+    Enter the roll no, m1, m2 : 
+    2
+    76
+    80
+
+        **********MARKSHEET OF DIPLOMA FIRST YEAR**********
+
+
+ROLL NO         MATHS           ENGLISH         TOTAL           PERCENTAGE
+
+
+1               85              94              179             89.50
+
+
+2               76              80              156             78.00
+
+*/
