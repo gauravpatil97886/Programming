afcec990f84584a5752da6eb9294da673245d281
Durwa <65773424+Durwa@users.noreply.github.com>
2021-01-25 16:26:45 +0530

    calculate the area of circle

---
+++ Durwapatil/Java/area-of-circle.java
@@ -0,0 +1,33 @@
+// Date : 25 Jan 2021 
+
+// Java program to display the area of circle 
+
+
+
+            import java.io.*;
+            class circle
+            {
+              public static void main(String args[])
+              {
+                try 
+                {
+                  DataInputStream x = new DataInputStream(System.in);
+                  int r;
+                  float ac;
+                  System.out.print("\nEnter radius : \n");
+                  r = Integer.parseInt(x.readLine());
+                  ac = (float)3.14 * r * r;
+                  System.out.print("\nThe area of the circle is "+ ac);
+                }
+                catch(Exception e)
+                {}
+              }
+            }
+            
+/*
+Output : 
+    Enter radius : 
+    5
+    
+    The area of the circle is 78.5
+*/
