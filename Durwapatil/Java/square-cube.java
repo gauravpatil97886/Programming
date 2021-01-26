5be1b183aae001323855a26de57f52b3d4f19a7b
Durwa <65773424+Durwa@users.noreply.github.com>
2021-01-26 20:12:09 +0530

    display the square and cube of a given number

---
+++ Durwapatil/Java/square-cube.java
@@ -0,0 +1,25 @@
+// Date: 26 Jan 2021 
+
+// Java program to display the square and cube of given number
+
+
+
+            class square
+            {
+              public static void main(String args[])
+              {
+                int a, s, c;
+                a = 5;
+                s = a * a;
+                System.out.print("Square : "+ s + "\n");
+                c = a * a * a;
+                System.out.print("Cube : " + c);
+              }
+            }
+            
+            
+/*
+Output : 
+    Square : 25
+    Cube : 125
+*/
